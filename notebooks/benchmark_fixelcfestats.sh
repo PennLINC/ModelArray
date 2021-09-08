af3dba0b1cc08099b401be0b85a845584318959e
zhao-cy <chenyingz@outlook.com>
2021-09-08 12:33:59 -0400

    add nthreads; add vmware

--- notebooks/benchmark_fixelcfestats.sh
+++ notebooks/benchmark_fixelcfestats.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-while getopts S:h:f:F:o:M:s: flag
+while getopts S:h:t:f:F:o:M:s:w: flag
 do
     case "${flag}" in
         S) num_subj=${OPTARG};;
         h) nshuffles=${OPTARG};;
+        t) nthreads=${OPTARG};;   # number of threads in fixelcfestats
         f) ftests=${OPTARG};;   # TRUE or FALSE
         F) fonly=${OPTARG};;   # TRUE or FALSE
         o) folder_output=${OPTARG};;
         M) run_memoryProfiler=${OPTARG};;   # "TRUE" or "FALSE"
         s) sample_sec=${OPTARG};;
+        w) run_where=${OPTARG};;  # "interactive" or "vmware"
     esac
 done
 
-folder_main_data="/cbica/projects/fixel_db/dropbox/data_from_josiane/for_fixelcfestats"
 
-source /cbica/projects/fixel_db/miniconda3/etc/profile.d/conda.sh   # found by $ conda info | grep -i 'base environment'
-conda activate
+if [[ "$run_where" == "interactive"  ]]; then
+    folder_main_data="/cbica/projects/fixel_db/dropbox/data_from_josiane/for_fixelcfestats"
+
+    source /cbica/projects/fixel_db/miniconda3/etc/profile.d/conda.sh   # found by $ conda info | grep -i 'base environment'
+    conda activate
+
+elif [[ "$run_where" == "vmware"  ]]; then
+    folder_main_data="/home/chenying/Desktop/fixel_project/data/data_from_josiane/for_fixelcfestats"
+fi
+
 
 fn_myMemProf="${folder_output}/output_myMemoryProfiler.txt"
 
@@ -26,6 +35,7 @@ cmd+=" ${folder_main_data}/designMatrix_lm_Age_n${num_subj}.txt"
 cmd+=" ${folder_main_data}/contrastMatrix_lm_n${num_subj}.txt"
 cmd+=" ${folder_main_data}/../MRtrix_conn_matrix/matrix/"
 cmd+=" $folder_output"
+cmd+=" -nthreads $nthreads"
 cmd+=" -nshuffles $nshuffles"
 
 if [[ "$ftests" == "TRUE"   ]]; then
