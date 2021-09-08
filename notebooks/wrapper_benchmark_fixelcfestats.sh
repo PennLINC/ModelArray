4ddd5e45e4799863a3a11d0a664b707a230335fa
zhao-cy <chenyingz@outlook.com>
2021-09-08 12:35:06 -0400

    add nthreads; add vmware

--- notebooks/wrapper_benchmark_fixelcfestats.sh
+++ notebooks/wrapper_benchmark_fixelcfestats.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # examples:
-# bash wrapper_benchmark_fixelcfestats.sh -S 30 -h 100 -f TRUE -F TRUE -w interactive -M TRUE -s 1
+# bash wrapper_benchmark_fixelcfestats.sh -S 938 -h 100 -t 2 -f TRUE -F TRUE -w vmware -M TRUE -s 0.1
 
-while getopts S:h:f:F:w:M:s: flag
+while getopts S:h:t:f:F:w:M:s: flag
 do
     case "${flag}" in
         S) num_subj=${OPTARG};;
         h) nshuffles=${OPTARG};;
+        t) nthreads=${OPTARG};;   # number of threads in fixelcfestats
         f) ftests=${OPTARG};;   # TRUE or FALSE
         F) fonly=${OPTARG};;   # TRUE or FALSE
-        w) run_where=${OPTARG};;   # TRUE or FALSE
+        w) run_where=${OPTARG};;   # "interactive" or "vmware"
         M) run_memoryProfiler=${OPTARG};;   # "TRUE" or "FALSE"
         s) sample_sec=${OPTARG};;
     esac
@@ -19,8 +20,13 @@ done
 printf -v date '%(%Y%m%d-%H%M%S)T' -1   # $date, in YYYYmmdd-HHMMSS
 echo "date variable: ${date}"
 
-folder_main_output="/cbica/projects/fixel_db/dropbox/data_from_josiane/for_fixelcfestats/stats_FDC"
-folder_output="${folder_main_output}/n-${num_subj}.nshuffles-${nshuffles}"
+if [[ "$run_where" == "interactive"  ]]; then
+    folder_main_output="/cbica/projects/fixel_db/dropbox/data_from_josiane/for_fixelcfestats/stats_FDC"
+elif [[ "$run_where" == "vmware"  ]]; then
+    folder_main_output="/home/chenying/Desktop/fixel_project/data/data_from_josiane/for_fixelcfestats/stats_FDC"
+fi
+
+folder_output="${folder_main_output}/nsubj-${num_subj}.nthreads-${nthreads}.nshuffles-${nshuffles}"
 
 if [[ "$ftests" == "TRUE"   ]]; then
     folder_output+=".ftests"
@@ -31,7 +37,7 @@ if [[ "$fonly" == "TRUE"   ]]; then
     folder_output+=".fonly"
 fi
 
-folder_outputs+=".${run_where}"
+folder_output+=".${run_where}"
 
 if [[ "$run_memoryProfiler" == "TRUE"  ]]; then
         folder_output="${folder_output}.runMemProfiler"  
@@ -48,4 +54,4 @@ fn_output_txt="${folder_output}/output.txt"
 
 
 
-bash benchmark_fixelcfestats.sh -S $num_subj -h $nshuffles -f $ftests -F $fonly -o $folder_output -M ${run_memoryProfiler} -s ${sample_sec} > $fn_output_txt 2>&1 &
+bash benchmark_fixelcfestats.sh -S $num_subj -h $nshuffles -t $nthreads -f $ftests -F $fonly -o $folder_output -M ${run_memoryProfiler} -s ${sample_sec} -w ${run_where} > $fn_output_txt 2>&1 &
