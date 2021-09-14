d6e865d25b1970c6ffefca40eab3e8a589525ce6
zhao-cy <chenyingz@outlook.com>
2021-09-14 18:01:26 -0400

    updates

--- notebooks/run_wrapper_benchmark_fixelcfestats.sh
+++ notebooks/run_wrapper_benchmark_fixelcfestats.sh
@@ -3,7 +3,7 @@
 function wait_r_end {
     while :
     do
-        pid_r=`ps -aux | grep "[R] --no-echo --no-restore" | awk '{print $2}'`     # [R] is to remove result of grep search; "awk" is to return pid only
+        pid_r=`ps -aux | grep "[f]ixelcfestats " | awk '{print $2}'`     # [R] is to remove result of grep search; "awk" is to return pid only
         if [ -z "$pid_r" ]; then
             date
             echo "sleep for $1 sec..."
@@ -17,17 +17,9 @@ function wait_r_end {
 #bash wrapper_benchmark_FixelArray.lm.sh -s 1 -D test_n50 -f 1000 -S 50 -c 4 -w vmware -M TRUE
 
 
-wait_r_end 10   # in seconds
-date
-cmd="bash wrapper_benchmark_FixelArray.lm.sh -s 1 -D josiane -f 0 -S 938 -c 1 -w vmware -M TRUE"
-echo $cmd
-$cmd
-date
-
 wait_r_end 600   # in seconds
 date
-cmd="bash wrapper_benchmark_FixelArray.lm.sh -s 1 -D josiane -f 0 -S 938 -c 2 -w vmware -M TRUE"
+cmd="bash wrapper_benchmark_fixelcfestats.sh -S 100 -h 100 -t 4 -f TRUE -F FALSE -n FALSE -w vmware -M TRUE -s 1"
 echo $cmd
 $cmd
 date
-
